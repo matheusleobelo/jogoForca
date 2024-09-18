8bb1e610366c41504face8f8f7ca76fd95681fc0
matheusleobelo <bipoladogamer@gmail.com>
2024-09-18 13:45:43 -0300

    arquivo main

---
+++ main.js
@@ -0,0 +1,36 @@
+let readline = require('readline-sync')
+
+//Função valores aleatorios
+function getRandomIntInclusive(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
+let posicaoPalavra = palavras[getRandomIntInclusive(0,3)] 
+  
+let palavra = ["palavra1", "palavra2", "palavra3"]
+
+let tamanhoPalavra
+
+
+  function verificarletra(letra){
+    
+
+
+  }
+
+
+  function play(){
+    while (vidas>0){
+      let letra = readline("Digite uma letra :")
+      
+    }
+    if (vidas = 0)
+      console.log ( "Seu merd*, burro, perdeu");
+      conslotchange.log(" Deseja jogar de novo?" );
+      
+
+
+
+  }
